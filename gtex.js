7488a4ea010199df6e99b4f8396c081a4ae19c3c
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-15 08:50:16 -0400

    Update gtex.js

--- gtex.js
+++ gtex.js
@@ -13,6 +13,19 @@ gtex={
 
 gtex.created_at=Date()
 
+gtex.getSample= async function (q){ // get samples with q atributes
+    q=q||''
+    if(typeof(q)=='object'){
+        let qstr=''
+        Object.keys(q).forEach(k=>{
+            qstr+=`${k}=${q[k]}&`
+        })
+        q=qstr.slice(0,-1)
+    }
+    return await (await fetch('https://gtexportal.org/rest/v1/biobank/sample?format=json&'+q)).json()
+}
+
+
 window.onload=gtex.ui
 
 // let's make sure it can be imported in applications like https://observablehq.com/@episphere/gtex
